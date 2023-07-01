22b9a1ffbd2d262b0f58be1c4b79d5ec51e5f116
smuke <smuke@smuke.dev>
2023-06-30 22:03:49 -0700

    Add mouse move event listener

---
+++ index.js
@@ -0,0 +1,6 @@
+const buttonContainer = document.getElementById("button-container");
+const glow = buttonContainer.querySelector(".button-glow");
+
+buttonContainer.addEventListener("mousemove", (e) => {
+    console.log(e);
+});
