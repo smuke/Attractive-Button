651cc6f65beef4ae0f1edb17d28a519224594c22
smuke <smuke@smuke.dev>
2023-06-30 23:51:43 -0700

    Position glow to mouse position

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const buttonContainer = document.getElementById("button-container");
+const button = document.getElementById("glowing-button");
 const glow = buttonContainer.querySelector(".button-glow");
 
 buttonContainer.addEventListener("mousemove", (e) => {
-    console.log(e);
+    // Positions, 100 is half of glow size (200)
+    const left = e.clientX - buttonContainer.offsetLeft - 100;
+    const top = e.clientY - buttonContainer.offsetTop - 100;
+
+    // Position glow
+    glow.style.left = `${left}px`;
+    glow.style.top = `${top}px`;
 });
